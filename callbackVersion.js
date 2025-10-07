cf0ec0edc07816123ed7f6e8257d30c4f5aa3565
TJ <afzaltj81@gmail.com>
2025-10-07 15:38:36 -0400

    Update callbackVersion.js

--- callbackVersion.js
+++ callbackVersion.js
@@ -1,45 +1,86 @@
+function doSummerChores(name) {
 
-function mowLawn (callback) {
-  console.log('Mowing the lawn');
-  setTimeout(() => { 2000 });
-  callback();
-}
-
-function weedEater (callback) {
-  console.log('Weed eating the lawn');
-  setTimeout(() => { 20000 });
-  callback();
-}
-
-function trimHedges (callback) {
-  console.log('Trimming the hedges');
-  setTimeout(() => { 1000 });
-  callback();
-}
-
-function collectWodd (callback) {
-  console.log('Collecting the wood');
-  setTimeout(() => { 2500 });
-  callback();
-}
-function waterGradern (callback) {
-  console.log('Watering the garden');
-  setTimeout(() => { 500 });
-  callback();
-}
-
-function doSummerChores () {
-  mowLawn(() => {
-    weedEater(() => {
-      trimHedges(() => {
-        collectWodd(() => {
-          waterGradern(() => {
-            console.log('All done with the summer chores!');
+  mowLawn(name, () => {
+    weedEater(name, () => {
+      trimHedges(name, () => {
+        collectWood(name, () => {
+          waterGarden(name, () => {
+            console.log(`All done with ${name}'s summer chores! They didn't fall asleep once!`);
           });
         });
       });
     });
   });
+}
+
+function mowLawn(name, callback) {
+  console.log(`${name} is mowing the lawn`);
+  const fellAsleep = Math.random() > 0.7;
+
+  setTimeout(() => {
+    if (fellAsleep) {
+      console.log(`${name} fell asleep while mowing the lawn `); 
+    } else {
+      console.log(`${name} successfully mowed the lawn `);
+      callback(); 
+    }
+  }, 2000);
+}
+
+function weedEater(name, callback) {
+  console.log(`${name} is weed eating the lawn`);
+  const fellAsleep = Math.random() > 0.7;
+
+  setTimeout(() => {
+    if (fellAsleep) {
+      console.log(`${name} fell asleep while weed eating the lawn `);
+    } else {
+      console.log(`${name} successfully weed ate the lawn `);
+      callback(); 
+    }
+  }, 2000);
+}
+
+function trimHedges(name, callback) {
+  console.log(`${name} is trimming the hedges`);
+  const fellAsleep = Math.random() > 0.7;
+
+  setTimeout(() => {
+    if (fellAsleep) {
+      console.log(`${name} fell asleep while trimming hedges `);
+    } else {
+      console.log(`${name} successfully trimmed the hedges `);
+      callback(); 
+    }
+  }, 7000);
+}
+
+function collectWood(name, callback) {
+  console.log(`${name} is collecting the wood`);
+  const fellAsleep = Math.random() > 0.7;
+
+  setTimeout(() => {
+    if (fellAsleep) {
+      console.log(`${name} fell asleep while collecting wood `); 
+    } else {
+      console.log(`${name} successfully collected wood `);
+      callback(); 
+    }
+  }, 2500);
+}
+
+function waterGarden(name, callback) {
+  console.log(`${name} is watering the garden`);
+  const fellAsleep = Math.random() > 0.7;
+
+  setTimeout(() => {
+    if (fellAsleep) {
+      console.log(`${name} fell asleep while watering garden `);
+    } else {
+      console.log(`${name} successfully watered garden `);
+      callback(); 
+    }
+  }, 500);
 }
 
 doSummerChores("Luna");
